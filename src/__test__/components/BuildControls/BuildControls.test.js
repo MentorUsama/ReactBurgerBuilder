10269588d6e5a06512663d1ac4f9a114a20c2007
MentorUsama <usama.farhat.99@gmail.com>
2022-10-06 23:24:09 +0500

    BuildControl debug removed

--- src/__test__/components/BuildControls/BuildControls.test.js
+++ src/__test__/components/BuildControls/BuildControls.test.js
@@ -1,7 +1,5 @@
 import {render, screen, } from '@testing-library/react';
 import BuildControls from '../../../components/BuildControls/BuildControl'
-import userEvent from '@testing-library/user-event';
-
 
 describe("<BuildControls /> testing",()=>{
     test("should show signup button when not authorize",()=>{
@@ -10,7 +8,6 @@ describe("<BuildControls /> testing",()=>{
     })
     test("should show order button when authorize",()=>{
         render(<BuildControls disabled={false} price={100} isAuth={true}/>)
-        screen.debug()
         expect(screen.getByText(/ORDER NOW/)).toBeInTheDocument()
     })
 })
